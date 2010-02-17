4b56391f96a7e846829d8d3b7f92dba2945e5ed8
Jeremy Deininger <jeremy@rightscale.com>
2010-02-16 17:22:15 -0800

    dur

--- cookbooks/nomachinenx/attributes/nomachinenx.rb
+++ cookbooks/nomachinenx/attributes/nomachinenx.rb
@@ -1,3 +1,3 @@
-set_unless nomachinenx[:user] = "nomachine"
-set_unless nomachinenx[:pass] = "guest"
-set_unless nomachinenx[:ssh_port] = "22"
+set_unless[:nomachinenx][:user] = "nomachine"
+set_unless[:nomachinenx][:pass] = "guest"
+set_unless[:nomachinenx][:ssh_port] = "22"
